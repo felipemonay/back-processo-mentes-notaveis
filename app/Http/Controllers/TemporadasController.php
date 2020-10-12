ecee22480095884361a59bfb2cd9fa40f13afdb1
Felipe Monay <felipemonay@gmail.com>
2020-10-11 21:58:42 -0300

    laravel

---
+++ app/Http/Controllers/TemporadasController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Serie;
+use Illuminate\Http\Request;
+
+class TemporadasController extends Controller
+{
+    public function index(int $serieId){
+        $serie = Serie::find($serieId)->temporadas;
+        $temporadas = $serie->temporadas;
+        return view('temporadas.index', compact('temporadas'));
+    }
+}
